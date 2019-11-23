8c8adeb86de855709bd0c08c70848860909ad611
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-11-23 11:45:19 +0530

    [TESTS] Added test cases for return statement

--- test/core.test.js
+++ test/core.test.js
@@ -355,4 +355,26 @@ describe('Core builder api', function() {
     assert.strictEqual(code, output);
 
   });
+
+  it('should generate a return statement', function() {
+
+    const fixturePath = 'test/fixtures/returnStatement';
+    const inputFixture = `${fixturePath}.input.js`;
+    const outputFixture = `${fixturePath}.output.js`;
+    const input = fs.readFileSync(inputFixture, 'utf-8');
+    let ast = parse(input);
+
+    let pseudoAst =  buildAST(ast);
+    const sampleCode = '';
+    const outputAst = parse(sampleCode);  
+
+    // Check the manifested api is working fine
+    pseudoAst.forEach(n => outputAst.program.body.push(eval(n)));
+
+    const code = print(outputAst, { quote: 'single'}).code;
+    const output = fs.readFileSync(outputFixture, 'utf-8');
+
+    assert.strictEqual(code, output);
+
+  });
 });
---
+++ test/fixtures/returnStatement.output.js
@@ -0,0 +1,3 @@
+return 1;
+return true;
+return "hello";
