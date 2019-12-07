540962d3169087ebbe71c9683bfafd2220252eb9
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-12-07 18:25:07 +0530

    [CHORE] Adding test cases for JSX builders

--- test/fixtures/jsx/element.input.js
+++ test/fixtures/jsx/element.input.js
@@ -2,3 +2,31 @@ const profile = <div>
   <img src="avatar.png" className="profile" />
   <h3>{[user.firstName, user.lastName].join(' ')}</h3>
 </div>;
+
+var React = require('react/addons');
+
+<React.addons.CSSTransitionGroup>
+  {''}
+</React.addons.CSSTransitionGroup>;
+
+var React = require('React');
+
+<Foo>
+  {children}
+</Foo>;
+<Foo>
+  {firstChild}
+  {otherChildren}
+</Foo>;
+
+var React = require('react/addons');
+
+<div>
+  {foo()}
+</div>;
+
+var React = require('react/addons');
+
+<div {...getProps()}>
+  foo
+</div>;
