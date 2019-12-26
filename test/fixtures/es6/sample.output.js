22d9ad66cd0dae9f5933ae1bdb6296c9743fa9e0
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-12-26 13:24:19 +0530

    [CHORE] Adding conditional expression to return statement

---
+++ test/fixtures/es6/sample.output.js
@@ -0,0 +1,19 @@
+export default class Foo extends Component {
+  bar;
+
+  @tracked
+  baz = 'barBaz';
+
+  @alias('model.isFoo')
+  isFoo;
+
+  @computed('isFoo')
+  get bazInfo() {
+    return get(this, 'isFoo') ? `Name: ${get(this, 'baz')}` : 'Baz';
+  }
+
+  @computed('bar', 'isFoo').readOnly()
+  get barInfo() {
+    return get(this, 'isFoo') ? `Name: ${get(this, 'bab')}` : 'Bar';
+  }
+}
